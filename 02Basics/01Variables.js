d393c227c42733023d7a660775326307923d88c7
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-09 14:03:43 +0530

    variable code is added

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,15 @@
+var fullName = "Atharva Mahamuni";
+console.log(fullName);
+
+var booleanValue = true;
+console.log(booleanValue);
+
+var number; //variable declaration
+console.log(number); //output will be undefined because no. is not assigned
+
+number = 55; //variable initialization
+console.log(number);
+
+var courseName = "javaScript course";
+
+//var if = 22;  //we can't use if becasue it is one of the keyword in js
