5c04c09310b3cb6e948d95c4ce093f9dc319e73f
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-15 12:15:55 +0530

    Using logical operators to make purchase decision.

---
+++ 02Basics/06LogicalCondition.js
@@ -0,0 +1,26 @@
+//we want to make user purchase from website
+//3 things to verify
+//user is logged in or not
+//user is verified email or not
+//card info is valid or not
+
+var loggedIn = true;
+var emailVerified = true;
+var cardValid = true;
+
+// if (loggedIn) {
+//   console.log("You are successfully logged in.");
+//   if (emailVerified) {
+//     console.log("Your email is verified");
+//     if (cardValid) {
+//       console.log("Your card id valid, now you can make the purchase");
+//     }
+//   }
+// }
+
+//Using &&
+if (loggedIn && emailVerified && cardValid) {
+  console.log("Everything is verified, now you can make the purchase.");
+} else {
+  console.log("Sorry, you can't make the purchase.");
+}
