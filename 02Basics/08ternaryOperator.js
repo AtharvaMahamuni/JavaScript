9bb6d53a99d24fd64fd83abdb6fca5dd0e0edb88
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-17 17:41:39 +0530

    Using ternary operator for sign in and sign out

---
+++ 02Basics/08ternaryOperator.js
@@ -0,0 +1,6 @@
+// If user is authenticated show him signout button
+// If user is not authonticated show him signIn/signUp button
+
+var userAuthentication = true;
+
+userAuthentication ? console.log("Sign Out") : console.log("Sign In");
