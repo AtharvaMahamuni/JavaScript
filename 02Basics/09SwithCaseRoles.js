f25a3b230f57a44bc1cb1ce9019d2fcc0c6d5e26
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-18 14:31:49 +0530

    switch case with site role example

---
+++ 02Basics/09SwithCaseRoles.js
@@ -0,0 +1,27 @@
+//We want to assign role as per the user who log in
+//admin - He have access of everything
+//subadmin - He can create and delete the cousrses
+//testPrep - He can create and delete the tests
+//student - He can consume the content
+//trailUser - Just come to explore
+
+var role = "testPrep";
+
+switch (role) {
+  case "admin":
+    console.log("You have all access.");
+    break;
+  case "subadmin":
+    console.log("You can create and delete courses");
+    break;
+  case "testPrep":
+    console.log("You can create and delete tests");
+    break;
+  case "student":
+    console.log("You can consume the content");
+    break;
+
+  default:
+    console.log("You are just trial user.");
+    break;
+}
