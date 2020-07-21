fe935940859d1127ac9402cc8b6698e81375a696
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-21 18:46:22 +0530

    functions in python

---
+++ 03Intermediate/01function.js
@@ -0,0 +1,15 @@
+function hello(name) {
+  console.log("Hello, ", name);
+  console.log(`Hello, ${name}`);
+}
+
+function namaste() {
+  return "Welcome in India";
+}
+
+var msg = namaste();
+console.log(msg);
+console.log(namaste());
+
+// hello();
+// hello("atharva");
