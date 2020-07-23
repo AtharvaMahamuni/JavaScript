8416aecbcfe79680817fc32957238dfa632be473
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-23 16:07:58 +0530

    comment answer update

--- 03Intermediate/03jsContext.js
+++ 03Intermediate/03jsContext.js
@@ -15,3 +15,8 @@
 //It run perfectly fine
 //It is due to browser has window as a global context
 //But node has different global context
+
+//In node , the window object is renamed  as global.
+//In simpler words, just like in browser there is a window object in
+//which all the global variables reside,in node there is "global" object
+//in which all the global variables reside.
