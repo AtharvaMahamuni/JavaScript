dcaf3a97d690dfabf6c3ee05e92acf79b96a3b74
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-23 16:01:40 +0530

    context in java script

---
+++ 03Intermediate/03jsContext.js
@@ -0,0 +1,17 @@
+// sayHello();
+
+// function sayHello() {
+//   console.log("Hello, Everyone!");
+// }
+//First function call is acceptable in javaScript due to js context
+//which already loaded the function as a global context
+//that is why js is aware about all the functions
+
+// var myName = "Atharva";
+// if (myName === window.myName) {
+//   console.log("It is true");
+// }
+//This code will give error in IDE but when you run it on browser console
+//It run perfectly fine
+//It is due to browser has window as a global context
+//But node has different global context
