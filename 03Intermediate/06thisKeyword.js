770d8663a791faa915c8c94db5e26177fcd262ba
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-27 20:08:39 +0530

    this keyword in java script

---
+++ 03Intermediate/06thisKeyword.js
@@ -0,0 +1,17 @@
+//TODO: This keyword part 1
+
+console.log(this);
+
+/*****
+ * We have this keyword which give us the global context
+ * Global context differs as per the situation
+ * in node it shows the empty scope and in browser it shows access of windows
+ *
+ */
+
+// function sayName() {
+//   console.log("Athrva here!!!");
+//   console.log(this);
+// }
+
+// sayName();
