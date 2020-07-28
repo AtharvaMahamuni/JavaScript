3c83a2d00ee5db7d6289794a5a88d716beca0087
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-07-28 20:19:27 +0530

    array code is added

---
+++ 03Intermediate/07array.js
@@ -0,0 +1,20 @@
+var countries = ["India", "USA", "Canada", "Russia"];
+
+var cities = new Array("Solapur", "Mumbai", "Delhi", "Pune", "Jaipur");
+
+// console.log(countries);
+// console.log(cities);
+
+// var user = ["Atharva", "athrva123@email.com", 21, 2020];
+
+// countries.push("Vakanda");
+// console.log(countries);
+// countries.pop();
+// console.log(countries);
+
+// countries.unshift(cities[0]);
+// console.log(countries);
+// countries.shift();
+// console.log(countries);
+
+// console.log(countries.fill(0));
