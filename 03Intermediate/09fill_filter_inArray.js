714c023c08bf872892df45424dfda4dbf4dd21d6
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2020-08-03 11:46:55 +0530

    fill and filter methods in array

---
+++ 03Intermediate/09fill_filter_inArray.js
@@ -0,0 +1,11 @@
+var testArray = [2, 4, 6, 3, 1, 5, 9, 8];
+
+console.log(testArray.fill("empty", 2, 5));
+
+// console.log(testArray);
+
+const myNumber = [23, 24, 25, 55, 66, 77, 87, 98];
+
+const result = myNumber.filter((num) => num > 55); //!= 55);
+
+console.log(result);
