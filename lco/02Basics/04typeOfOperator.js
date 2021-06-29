ffeb16ecf1fb09c9ddfb2a40dfa596afc9d4bb34
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-06-29 15:51:16 +0530

    new

--- lco/02Basics/04typeOfOperator.js
+++ lco/02Basics/04typeOfOperator.js
@@ -7,7 +7,7 @@ var value = true;
 console.log(typeof value); //boolean
 
 
-var num = 5;
+var num = 5;  
 console.log(typeof num); //number
 
 // typeof is one of the operator which give us the type of variable.
