de660203a7a1fdf398d6ef5a7687796beddba808
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-07-06 23:30:10 +0530

    edit

--- 07 ISH advance/02strings.js
+++ 07 ISH advance/02strings.js
@@ -1,11 +1,10 @@
-var firstName = `atharva`
-var fullName = `${firstName} mahamuni`
+var firstName = `Atharva`;
+var fullName = `${firstName} Mahamuni`; // when we do ${} it is known as interpolation
 
 console.log(fullName);
+
 
 console.log(fullName.charAt(0));
 console.log(fullName.endsWith("n"));
 console.log(fullName.includes("ath"));
-
-
 console.log(fullName.toUpperCase());
