dbd49cf6680262781fb77b41d1c81fe1e319f0f7
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-03-05 08:41:33 +0530

    cg js

---
+++ cg-js/01LAB/common.js
@@ -0,0 +1,11 @@
+var msg;
+msg="<p><code>The actual script is in external script file called common.js</code></p>";
+function addNos(headVar,bodyVar)
+{
+//TODO: display the contents of the variable "msg" 
+//TODO: display the addition of two numbers
+
+    document.write(msg);
+    document.write(headVar + bodyVar);
+
+}
