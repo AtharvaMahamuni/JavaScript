67591b3247e537bb675518b02a59f22c99c681ad
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-08-08 23:09:00 +0530

    reverse sentence code

--- DSA/02_Strings/01_ReverseSentence.js
+++ DSA/02_Strings/01_ReverseSentence.js
@@ -1,11 +1,16 @@
 // TODO: Here we have to reverse the sentence and not reverse the string.
 
-let string1 = '';
-let string2 = '';
-let string3 = '';
+let string1 = 'Hello my dear world';
+let string2 = 'It is nice sentence';
+let string3 = 'Hey';
 
 function reverseSentence(string) {
-    for(let i = 0; i < string.length; i++) {
-        
-    }
+    let splitString = string.split(' ');
+    let reverseString = splitString.reverse();
+    string = reverseString.join(' ');
+    console.log(string);
 }
+
+reverseSentence(string1);
+reverseSentence(string2);
+reverseSentence(string3);
