4a4046a2b2c31339a0f1301ca7e3e59061f115b6
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-08-12 22:01:12 +0530

    can sum problem

---
+++ DSA/FCC/Dynamic Programming/Memoization/03_canSum.js
@@ -0,0 +1,38 @@
+// TODO: Write the function which tells that the target sum is possible to generate or not from the given numbers in an array.
+
+// We can use the elements of the array as many times as needed.
+// Assume all input numbers are non-negative
+
+// for eg. canSum(7, [5, 3, 4, 7]) -> true
+// 3 + 4
+// 7
+
+// for eg. canSum(7, [2, 4]) -> false
+
+
+function canSum(targetSum, array, diary = {}) {
+
+    if(targetSum === 0) return true;
+    if(targetSum < 0) return false;
+
+    if(targetSum in diary) {
+        return diary[targetSum]
+    }
+
+    for(let num of array) {
+        let remainder = targetSum - num;
+        if(canSum(remainder, array, diary) === true) {
+            diary[targetSum] = true;
+            return diary[targetSum];
+        }
+    }
+
+    diary[targetSum] = false
+    return diary[targetSum];
+}
+
+console.log(canSum(7, [5, 3, 4, 7]));
+console.log(canSum(7, [2, 4]));
+console.log(canSum(8, [2, 3, 5]));
+console.log(canSum(300, [7, 14]));
+console.log(canSum(300, [2, 7, 14]));
