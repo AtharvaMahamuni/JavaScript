fbfada69c140991de3e8e17ed6b2ee25ab2f977e
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-08-26 15:47:12 +0530

    update

---
+++ Practice/velotioQuestion.js
@@ -0,0 +1,19 @@
+function arrayProduct(array) {
+
+}
+
+
+function cardGame(N, K) {
+    let array = new Array(N);
+    array.fill(1, 0, N);
+
+    let result = 0
+
+    for(let i = 0; i < N; i++) {
+        
+    }
+
+    return result;
+}
+
+cardGame(3, 6);
