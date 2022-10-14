51e7140aa5b79708a885d342498a51b90ad898cc
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-10-14 21:53:52 +0530

    JS

---
+++ S_1_Namaste_JavaScript/01_HowJSworks.js
@@ -0,0 +1,13 @@
+// Everything in JavaScript happens inside an execution context
+
+// There are two components in Execution context 
+// 1. Memory context (Variable environment)
+// 2. Code component (Thread of Execution)
+
+// Now becuase of Threaded Execution there is one more property of JavaScript which says ->
+// JavaScript is a synchronous single threaded language.
+// It means JS can execute 1 command at a time in a specific order.
+
+
+
+
---
+++ S_1_Namaste_JavaScript/02_HowJScodeExecute.js
@@ -0,0 +1,8 @@
+// What happens when you run the JS code?
+// When we run the JS program the execution context gets created.
+
+// In JS execution context there are 2 different components -
+// Memory and code component.
+// The execution context gets created in 2 phases -
+// 1. In 1st phase the memory component gets created 
+// 2. In 2nd phase the code component gets created.
